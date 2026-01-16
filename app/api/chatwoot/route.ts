1814c174a2d9efe3283d67d028dd5ad7c41519e6
claudiogarone <claudiogarone@gmail.com>
2026-01-16 21:27:22 +0100

    Fix codice finale

--- app/api/chatwoot/route.ts
+++ app/api/chatwoot/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 
 const CHATWOOT_URL = ''; const API_TOKEN = 'zeHgZgfbrKrEmSAmiqtnsyq9r'; const ACCOUNT_ID = '1';
 
-export async function GET() { try { const response = await fetch(${CHATWOOT_URL}/api/v1/accounts/${ACCOUNT_ID}/conversations?status=open, { headers: { 'api_access_token': API_TOKEN, 'Content-Type': 'application/json', }, cache: 'no-store' });
+export async function GET() { try { const response = await fetch(`${CHATWOOT_URL}/api/v1/accounts/${ACCOUNT_ID}/conversations?status=open`, { headers: { 'api_access_token': API_TOKEN, 'Content-Type': 'application/json', }, cache: 'no-store' });
 
 } catch (error: any) { return NextResponse.json({ error: error.message }, { status: 500 }); } }
